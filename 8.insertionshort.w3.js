e977b0a3f5a1d0aa9776980243c8e08df7c37bde
fadhil9 <fadhilmuhamad272@gmail.com>
2024-08-17 14:32:00 +0700

    gua mulai paham insertion short

--- 8.insertionshort.w3.js
+++ 8.insertionshort.w3.js
@@ -10,20 +10,20 @@
 
 function insertionShort(arr) {
     for(let i = 1; i< arr.length; i++){
-        let kunci = arr[i]  //b itu nilai elemen kedua : b=3
-        let kirikunci = i-1 //  a itu index sebelah kiri sebelum b , a = 0
+        let kunci = arr[i]  //nilai elemen array                //[4,5,9,(1)] kunci =1
+        let indexkirikunci = i-1      //index bagian kiri kunci  // 2
 
-        while(kirikunci>=0 && arr[kirikunci]>kunci){
-            
-            // arr[a+1] = arr[a]  
-            arr[a] = arr[a+1]  
-            a = a-1 
+        while(indexkirikunci>=0 && arr[indexkirikunci]>kunci){  //[4,5,(9>kunci=1),1] true, [4,(5>kunci=1),9,9] true
+                                                                //[(4>kunci=1),5,1,9] true , false 
+
+            arr[indexkirikunci+1] = arr[indexkirikunci]         //[4,5,(9,9)] , [4,(5,5),9]. [(4,4),5,9]
+            indexkirikunci= indexkirikunci-1                      //1, 0, -1
         }
 
-        arr[a+1] = b
+        arr[indexkirikunci+1] = kunci                           //[(1),4,5,9,)]
     }
     return arr
 }
 
-console.log(insertionShort([10,3,9]))
+console.log(insertionShort([4,5,9,1]))
 
