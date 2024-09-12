14235a6d5e46aa0ec2882b1aadc69aa87545d0bc
fadhil9 <fadhilmuhamad272@gmail.com>
2024-09-13 05:56:37 +0700

    update between two sets

--- 9.betweenTwoSets.hackerank.js
+++ 9.betweenTwoSets.hackerank.js
@@ -1,7 +1,10 @@
 function getTotalX(a,b) {
     function gcd(x,y) {
         while(y !== 0){
-            
+            let temp = y;
+            y = x % y;
+            x = temp;
         }
+        return x
     }    
 }
